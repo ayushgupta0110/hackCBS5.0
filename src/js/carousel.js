6bf8d312cd4f13f522ee200cb91939f4ea90e204
Pankaj Prajapati <paprajapati9@gmail.com>
2022-07-24 14:00:25 +0530

    minor change

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,33 +1,44 @@
 window.addEventListener('load', function() {
-    new Swiper(".swiper",{
-        effect: "carousel",
-        grabCursor: true,
-        loop: true,
-        loopedSlides: 3,
-        slidesPerView: 1.8,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        pagination: {
-            el: ".swiper-pagination"
-        },
-        watchSlidesProgress: true,
-        centeredSlides: true,
-        autoplay: {
-            delay: 3000
-        },
-        breakpoints: {
-            // when window width is >= 580px
-            580: {
-                slidesPerView: 1.2,
-                spaceBetween: 0
+    if(screen.width > 580) {
+        new Swiper(".swiper",{
+            effect: "carousel",
+            grabCursor: true,
+            loop: true,
+            loopedSlides: 3,
+            slidesPerView: 1.8,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
             },
-            // when window width is >= 640px
-            640: {
-                slidesPerView: 1.5,
-                spaceBetween: 0
+            pagination: {
+                el: ".swiper-pagination"
+            },
+            watchSlidesProgress: true,
+            centeredSlides: true,
+            autoplay: {
+                delay: 3000
             }
-        }
-    });
+        });
+    }
+    else {
+        new Swiper(".swiper",{
+            effect: "carousel",
+            grabCursor: true,
+            loop: true,
+            loopedSlides: 3,
+            slidesPerView: 1.3,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            },
+            pagination: {
+                el: ".swiper-pagination"
+            },
+            watchSlidesProgress: true,
+            centeredSlides: true,
+            autoplay: {
+                delay: 3000
+            }
+        });
+    }
 });
